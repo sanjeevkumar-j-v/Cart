ec8ff7f43b8d6fd3b89dfc51792c41032ada2a68
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-07-16 13:21:21 +0530

    Add state to cartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+// const element = <FontAwesomeIcon icon={faCoffee} />
+
+// ReactDOM.render(element, document.body)
+
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -9,11 +26,18 @@ class CartItem extends React.Component {
 
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25}}>Phone</div>
-                    <div style={{ color: 'red'}}>Rs. 999</div>
-                    <div style={{ color: 'red'}}>Qty: 1</div>
+                    <div style={{ fontSize: 25}}>{title}</div>
+                    <div style={{ color: 'red'}}>Rs. {price}</div>
+                    <div style={{ color: 'red'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        {/* <img alt="increase" className="action-icons" src="" />
+                        <img alt="decrease" className="action-icons" src="" />
+                        <img alt="delete" className="action-icons" src="" /> */}
+                        <div className="action-icons">{<FontAwesomeIcon icon={faPlus} />}</div>
+                        <div className="action-icons">{<FontAwesomeIcon icon={faMinus} />}</div>
+                        <div className="action-icons">{<FontAwesomeIcon icon={faTrash} />}</div>
+                    
                     </div>
                 </div>
             </div>
