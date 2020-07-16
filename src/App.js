88ad061436c4d4d8260434da5391065eace01ff9
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-07-16 23:39:45 +0530

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cart</h1>
-        
+        <Navbar/>
         <Cart/>
       </header>
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = (props) => {  
+    
+    return (
+        <div style={styles.nav}>
+            <div style={styles.cartIconContainer}>
+                <div style={ styles.cartIcon }>
+                    {<FontAwesomeIcon icon={faShoppingCart} />}
+                </div>
+                <span style={styles.cartCount}>3</span>
+            </div>
+        </div>
+    );
+
+}
+
+const styles = {
+    cartIcon: {
+        height: 32,
+        marginRight: 25,
+        fontSize: 40
+    },
+    nav: {
+        height: 70,
+        background: "violet",
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    cartIconContainer: {
+        position: 'relative'
+    },
+    cartCount: {
+        background: 'yellow',
+        borderRadius: '50%',
+        padding: '4px 8px',
+        position: 'absolute',
+        right: 10,
+        top:0
+    }
+}
+export default Navbar;
